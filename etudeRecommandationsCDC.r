a2c411d1aeacf75126a88164e1a80f5a7e261d06
scooby100 <39526462+scooby100@users.noreply.github.com>
2018-07-04 15:25:53 +0200

    Update etudeRecommandationsCDC.r

--- etudeRecommandationsCDC.r
+++ etudeRecommandationsCDC.r
@@ -1,31 +1,35 @@
  #ETUDE DES RECOMMANDATIONS DE LA COUR DES COMPTES#
  #Necessite le lancement de textClean.r pour la donction textClean()
+ writeout <- function(X,nom,affichage_noms_variables) {
+ write.table(X,file=str_c(nom,".csv"),sep=";",dec=",",row.names=affichage_noms_variables)
+ return()
+ }
  fichierSelectionne <- "TabRecommandationsCDC.csv"
  books_cdc <- read.csv(fichierSelectionne,sep=";",header=T,stringsAsFactors=FALSE) 
- colRecommandations <- books_cdc$LibellÈ.recommandation
+ colRecommandations <- books_cdc$Libell√©.recommandation
  colTypePubli       <- books_cdc$Type.de.publication 
- # transformation du data.frame par Èclatement des phrases de recommandations #
+ # transformation du data.frame par √©clatement des phrases de recommandations #
  #exemple:
  df <- data.frame(x=letters[runif(3,min=1,max=26)],
-					phrases=c("mon.chat a peur","caro.line n'est plus, dÈputÈe","olive.vert, pas mure"),
+					phrases=c("mon.chat a peur","caro.line n'est plus, d√©put√©e","olive.vert, pas mure"),
 					stringsAsFactors=FALSE)
  df %>% unnest(phrases=strsplit(phrases,"[,.' ]"))
  #fin exemple
  df <- books_cdc %>% 
-		unnest(LibellÈ.recommandation=strsplit(LibellÈ.recommandation,"[),.' ]"))
+		unnest(Libell√©.recommandation=strsplit(Libell√©.recommandation,"[),.' ]"))
  #Nettoyage du data frame des recommandations de la cour des comptes
  cdc_count <- books_cdc%>% 
-	mutate(text = stringr::str_replace_all(.$LibellÈ.recommandation,"í", " ")) %>%
-	unnest_tokens(LibellÈ.recommandation, text) %>%
-    filter(!LibellÈ.recommandation %in% stopwords('fr')) %>%
-	count(LibellÈ.recommandation, sort = TRUE)
-	############## nettoyage terminÈ ###############################
+	mutate(text = stringr::str_replace_all(.$Libell√©.recommandation,"‚Äô", " ")) %>%
+	unnest_tokens(Libell√©.recommandation, text) %>%
+    filter(!Libell√©.recommandation %in% stopwords('fr')) %>%
+	count(Libell√©.recommandation, sort = TRUE)
+	############## nettoyage termin√© ###############################
  pool <- textClean(str_c(colRecommandations,sep=" ",collapse=" "))
- pool <- str_replace(pool,"È","e")
+ pool <- str_replace(pool,"√©","e")
  ###  chargements des mots de liaisons inutiles ###
  library("stopwords") 
  motsinutiles <- stopwords(language = "fr") #classe caracteres
- mi <- motsinutiles #saisie simplifiÈ
+ mi <- motsinutiles #saisie simplifi√©
  pool <- setdiff(pool,motsinutiles) #bassin des mots du texte
  ###  fin procedure   mots de liaisons inutiles ###
  poool <- unique(pool)
@@ -34,7 +38,7 @@
  x2 <- stringdist("valorisation",poool,method="lv")
  x3 <- stringdist("valorisation",poool,method="jaccard")
  x4 <- stringdist("valorisation",poool,method="qgram")
- #data.frame Performances de similaritÈ textuelle en fonction des mÈthodes
+ #data.frame Performances de similarit√© textuelle en fonction des m√©thodes
 print("cosine:")
 print(x1[order(x1)[1:6]])
 print(str_c(t(poool[order(x1)][1:6])),collapse=" - ")
@@ -49,34 +53,34 @@ print(x4[order(x4)[1:6]])
 print(str_c(t(poool[order(x4)][1:6])),collapse=" - ")
 
 #######################################################################
-phrase <-  "faire Èvoluer le rÈgime de retraite des membres 
-vers un rÈgime ‡ cotisations dÈfinies,n'engageant pas l'Etat 
-au-del‡ du financement initialement consenti et applicable 
-‡ l'ensemble des pensions non encore liquidÈes"
-phrase1 <- "le rÈgime de retraite doit Èvoluer sur la base de 
-cotisations dÈfinies, qui n'engage pas l'Etat au-del‡ du financement 
-consenti au dÈpart et applicable ‡ l'ensemble des pensions non encore payÈes"
-phrase2 <- "maÓtriser rigoureusement les charges de fonctionnement des CLCC
-pour rÈtablir leur Èquilibre financier et leur capacitÈ ‡ investir, 
+phrase <-  "faire √©voluer le r√©gime de retraite des membres 
+vers un r√©gime √† cotisations d√©finies,n'engageant pas l'Etat 
+au-del√† du financement initialement consenti et applicable 
+√† l'ensemble des pensions non encore liquid√©es"
+phrase1 <- "le r√©gime de retraite doit √©voluer sur la base de 
+cotisations d√©finies, qui n'engage pas l'Etat au-del√† du financement 
+consenti au d√©part et applicable √† l'ensemble des pensions non encore pay√©es"
+phrase2 <- "ma√Ætriser rigoureusement les charges de fonctionnement des CLCC
+pour r√©tablir leur √©quilibre financier et leur capacit√© √† investir, 
 en particulier dans les centres les plus fragiles."
 print(stringdist(phrase,phrase1,method="jaccard"))
 #######################################################################
 #texte <- tolower(texte)
-#texte <- str_replace(texte,"È","e")
-#texte.words <- unlist(strsplit(texte,"[[:blank:].-]+")) #dÈcomposition du texte en mots 
+#texte <- str_replace(texte,"√©","e")
+#texte.words <- unlist(strsplit(texte,"[[:blank:].-]+")) #d√©composition du texte en mots 
 #texte.words <- na.omit(str_match(texte.words,"[^0-9]+")) #Nettoyage du texte des chiffres mais renvoie un type matrix
-#texte.words <- as.character(na.omit(str_match(texte.words,"[a-zA-zÈËÁ‡]{3,18}"))) #EnlËvement de mots de liaison non significatifs
+#texte.words <- as.character(na.omit(str_match(texte.words,"[a-zA-z√©√®√ß√†]{3,18}"))) #Enl√®vement de mots de liaison non significatifs
 
 ########################### package dplyr ############################################
-# transfomation du books_cdc : colonne 3 : titres des recommandations nettoyÈs par textClean()
+# transfomation du books_cdc : colonne 3 : titres des recommandations nettoy√©s par textClean()
 # description du books_cdc
 # col 2 : Type de publication / col 3 : Titre / col 4 : recommandations / col 5 : destinataire 1
 library(dplyr)
 df <- books_cdc
 df[,4] <- sapply(1:nrow(books_cdc),function(i) {books_cdc[i,4] <- tolower(books_cdc[i,4])})
 #df[,4] <- str_c(strsplit(df[,4],"[[:blank:].-]+"),collapse=" ")
-# manipulation du type tibble du package dplyr. Ce type est un data.frame spÈcial dont les variables peuvent Ítre des listes !! 
-df_bb <- (tibble(publication=df[,2],titre=as.list(df[,3]),destination=df[,5])) #construction d'un data.frame spÈcial (
+# manipulation du type tibble du package dplyr. Ce type est un data.frame sp√©cial dont les variables peuvent √™tre des listes !! 
+df_bb <- (tibble(publication=df[,2],titre=as.list(df[,3]),destination=df[,5])) #construction d'un data.frame sp√©cial (
 xtitres <- lapply(df_bb$titre,textClean) #nettoyage des titres
 xtitres <- lapply(xtitres,function(i) {str_c(i,collapse=" ")})
 df_bb$titre <- unlist(xtitres)
